ddb1be3b9c0b47eb6d333fe86aef7015aa9c81d9
ArletteUribe <arlette.uribe@iteso.mx>
2023-11-16 10:11:48 -0600

    empty main file added

---
+++ main.c
@@ -0,0 +1,20 @@
+/**
+ * @file    Project_DrMario.c
+ *
+ * @author	Arlette Uribe & Pablo Ramos
+ *
+ * @brief   Application entry point.
+ */
+
+#include <stdio.h>
+#include "MK64F12.h"
+
+
+int main(void) {
+
+
+    while(1) {
+
+    }
+    return 0 ;
+}
